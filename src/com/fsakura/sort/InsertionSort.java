0466bd08baf68ed2b715cabc4c41ceb345b4e202
Sakura <fukazawa.sakura@gmail.com>
2014-09-11 06:17:22 -0700

    insertion sort

---
+++ src/com/fsakura/sort/InsertionSort.java
@@ -0,0 +1,16 @@
+package com.fsakura.sort;
+
+public class InsertionSort {
+
+	public void sort(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			int temp = arr[i];
+			int j;
+			for (j = i - 1; j >= 0 && temp < arr[j]; j--) {
+				arr[j + 1] = arr[j];
+			}
+			arr[j + 1] = temp;
+		}
+	}
+
+}
