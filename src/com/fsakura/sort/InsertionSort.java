3f9a931acf85a580f9fc95795029d3e60516f8ef
Sakura <fukazawa.sakura@gmail.com>
2014-09-11 06:30:40 -0700

    indent

--- src/com/fsakura/sort/InsertionSort.java
+++ src/com/fsakura/sort/InsertionSort.java
@@ -2,15 +2,15 @@ package com.fsakura.sort;
 
 public class InsertionSort {
 
-  public void sort(int[] arr) {
-    for (int i = 0; i < arr.length; i++) {
-      int temp = arr[i];
-      int j;
-      for (j = i - 1; j >= 0 && temp < arr[j]; j--) {
-        arr[j + 1] = arr[j];
-      }
-      arr[j + 1] = temp;
+    public void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int temp = arr[i];
+            int j;
+            for (j = i - 1; j >= 0 && temp < arr[j]; j--) {
+                arr[j + 1] = arr[j];
+            }
+            arr[j + 1] = temp;
+        }
     }
-  }
 
 }
--- src/com/fsakura/sort/Main.java
+++ src/com/fsakura/sort/Main.java
@@ -1,13 +1,13 @@
 package com.fsakura.sort;
 
 public class Main {
-  public static void main(String[] args) {
-    InsertionSort is = new InsertionSort();
-    int[] arr = { 1, 5, 6, 2, 4, 5, 7, 7, 34, 3, 79, 25, 26, 19, -1, 4 };
-    is.sort(arr);
-    for (int i : arr) {
-      System.out.print(i + " ");
+    public static void main(String[] args) {
+        InsertionSort is = new InsertionSort();
+        int[] arr = { 1, 5, 6, 2, 4, 5, 7, 7, 34, 3, 79, 25, 26, 19, -1, 4 };
+        is.sort(arr);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println("");
     }
-    System.out.println("");
-  }
 }
